a4338fa47d1e65a5188f88f17ca18967c97f809f
Omar Saad <omarsaad@microsoft.com>
2026-02-09 20:09:07 +0200

    Fix login button icon and text vertical alignment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -73,7 +73,7 @@ function App() {
               <rect x="1" y="11" width="9" height="9" fill="#00a4ef"/>
               <rect x="11" y="11" width="9" height="9" fill="#ffb900"/>
             </svg>
-            Login with Microsoft
+            <span>Login with Microsoft</span>
           </a>
           <a href="/.auth/login/google?post_login_redirect_uri=/" className="btn btn-secondary btn-large btn-login" style={{ marginTop: '1rem' }}>
             <svg className="login-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -82,7 +82,7 @@ function App() {
               <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
               <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
             </svg>
-            Login with Google
+            <span>Login with Google</span>
           </a>
         </div>
       </div>
